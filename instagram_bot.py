249f8a13f3a993bfe8e7bd012f92ae0265a6b04b
deadsplash <deadsplashmail@gmail.com>
2021-09-11 00:06:22 +0300

    small changer that makes it look more understandable

--- instagram_bot.py
+++ instagram_bot.py
@@ -4,6 +4,12 @@ from auth_data import username, password
 import time
 import random
 
+print('Getting username and password and logging in Instagram.')
+time.sleep(1)
+print("Please, don't close Chrome window! It will proceed by itself.")
+time.sleep(1)
+print("Open this console after logging in is done!")
+time.sleep(3)
 browser = webdriver.Chrome('./chromedriver')
 
 
@@ -15,7 +21,6 @@ def exit_from_browser():
 
 
 def login(username, password):
-    global browser  # no need to define it each time
     try:
         browser.get('https://www.instagram.com/')
         time.sleep(random.randrange(3, 5))
@@ -31,18 +36,22 @@ def login(username, password):
         password_input.send_keys(password)
 
         password_input.send_keys(Keys.ENTER)
-        time.sleep(10)
-        # now we logged in
-        # browser.close()
-        # browser.quit()
+        time.sleep(5)
+
+        print('''
+        //////////////////////////////////////////////////
+        Check if you are logged in before choosing options.
+        //////////////////////////////////////////////////
+        ''')
+        time.sleep(5)
+
     except Exception as ex:
-        print(ex)
+        print(f'Something went wrong: {ex}')
         browser.close()
         browser.quit()
 
 
-def hashtag_search(hashtag, rolls=5):
-    global browser
+def hashtag_search(hashtag, exit_check,rolls=5):
     try:
         browser.get(f'https://www.instagram.com/explore/tags/{hashtag}/')
         time.sleep(5)
@@ -62,19 +71,34 @@ def hashtag_search(hashtag, rolls=5):
                 posts_urls.append(href)
 
         print(f'successfully parced urls for #{hashtag} hashtag, now bot will like posts')
+        count = 0
         for url in posts_urls:
-            try:
-                browser.get(url)
-                time.sleep(5)
-                like_button = browser.find_element_by_xpath(
-                    '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button').click()
-                time.sleep(random.randrange(80, 100))
-            except Exception as ex:
-                print(ex)
-                exit_from_browser()
+            if 0 < count < 60:
+                try:
+                    browser.get(url)
+                    time.sleep(5)
+                    like_button = browser.find_element_by_xpath(
+                        '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button').click()
+                    time.sleep(random.randrange(80, 100))
+                    count += 1
+
+                except Exception as ex:
+                    print(ex)
+                    exit_from_browser()
+            else:
+                pass
 
             print("bot liked:   ", url)
+        print(f"Number of posts liked:  {count}")
 
+        if exit_check == "Y":
+            browser.close()
+            browser.quit()
+            return False
+        elif exit_check == "N":
+            pass
+        else:
+            print('Something went wrong:  exit_check is incorrect.')
         # browser.close()
         # browser.quit()
     except Exception as ex:
@@ -83,10 +107,47 @@ def hashtag_search(hashtag, rolls=5):
         browser.quit()
 
 
-login(username, password)
+def main():
+    login(username, password)
+    while True:
+        print("""
+    Please, choose option from the list: 
+    
+1. Hashtag liker
+2. *** / work in progress
+3. *** / work in progress
+4. *** / work in progress
+0. Exit
+        """)
+        try:
+            inp = int(input())
+        except Exception as ex:
+            print('Wrong input, please try again.')
+            break
+        if inp == 1:
+            hashtag = str(input('Please, enter hash tag you want to try:  '))
+            print("Do you want to close browser after everything is done? Y/N")
+            exit_check = str(input()).upper()
+            if exit_check == 'Y' or exit_check == 'N':
+                print(f'OK, posting likes on #{hashtag}, please do not stop the program.')
+                time.sleep(3)  # small pause so user can read what you wrote in console
+                hashtag_search(hashtag, exit_check, rolls=1)
+            else:
+                print('Incorrect inputs, please try again.')
+        elif inp == 2:
+            print('Not available now.')
+            pass
+        elif inp == 3:
+            print('Not available now.')
+            pass
+        elif inp == 4:
+            print('Not available now.')
+            pass
+        elif inp == 0:
+            browser.close()
+            browser.quit()
+            print('App is shutting down.')
+            return False
 
 
-hashtag_search(str(input('Please, enter hash tag you want to try:  ')), rolls=1)
-
-browser.close()
-browser.quit()
+main()
