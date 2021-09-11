01ee5eeea28e63b6731bbc066bf0493f6dfb34dc
deadsplash <deadsplashmail@gmail.com>
2021-09-11 16:08:41 +0300

    v0.1.1 SNAPSHOT

--- instagram_bot.py
+++ instagram_bot.py
@@ -4,6 +4,16 @@ from auth_data import username, password
 import time
 import random
 
+"""
+
+INSTAGRAM BOT
+
+Important! Please, don't forget to add user login data to 'auth_data.py'.
+After logging in is done, you can work with a program.
+
+"""
+
+# Telling user what's happening
 print('Getting username and password and logging in Instagram.')
 time.sleep(1)
 print("Please, don't close Chrome window! It will proceed by itself.")
@@ -38,17 +48,41 @@ def login(username, password):
         password_input.send_keys(Keys.ENTER)
         time.sleep(5)
 
+        # we let user to check if everything is OK, and then come back
         print('''
         //////////////////////////////////////////////////
         Check if you are logged in before choosing options.
         //////////////////////////////////////////////////
         ''')
-        time.sleep(5)
+        time.sleep(7)
 
     except Exception as ex:
         print(f'Something went wrong: {ex}')
         browser.close()
         browser.quit()
+
+
+def like_machine(posts_urls):
+    # you'd better not like more that 60 posts a day, in case of getting banned for suspicious activity
+    count = 0
+    for url in posts_urls:
+        if 0 < count < 60:
+            try:
+                browser.get(url)
+                time.sleep(5)
+                like_button = browser.find_element_by_xpath(
+                    '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button').click()
+                time.sleep(random.randrange(80, 100))
+                count += 1
+
+            except Exception as ex:
+                exit_from_browser()
+                print(ex)
+        else:
+            pass
+
+        print("bot liked:   ", url)
+    print(f"Number of posts liked:  {count}")
 
 
 def hashtag_search(hashtag, exit_check,rolls=5):
@@ -71,25 +105,8 @@ def hashtag_search(hashtag, exit_check,rolls=5):
                 posts_urls.append(href)
 
         print(f'successfully parced urls for #{hashtag} hashtag, now bot will like posts')
-        count = 0
-        for url in posts_urls:
-            if 0 < count < 60:
-                try:
-                    browser.get(url)
-                    time.sleep(5)
-                    like_button = browser.find_element_by_xpath(
-                        '//*[@id="react-root"]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button').click()
-                    time.sleep(random.randrange(80, 100))
-                    count += 1
 
-                except Exception as ex:
-                    print(ex)
-                    exit_from_browser()
-            else:
-                pass
-
-            print("bot liked:   ", url)
-        print(f"Number of posts liked:  {count}")
+        like_machine(posts_urls)
 
         if exit_check == "Y":
             browser.close()
